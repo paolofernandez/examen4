51dcbe1c746e2b05f41cde79ca2508ba2f216d19
Paolo Adrian Fernandez Roman <piro_pafr@hotmail.com>
2013-12-03 08:26:37 -0400

    Respuesta 4.1

---
+++ src/main/java/Anho.java
@@ -0,0 +1,34 @@
+
+public class Anho {
+	private int anho;
+	
+	public Anho(){
+		anho=-1;
+	}
+	
+	public Anho(int anho){
+		this.anho = anho;
+	}
+	
+	public int getAnho() {
+		return anho;
+	}
+
+	public void setAnho(int anho) {
+		this.anho = anho;
+	}
+	
+	public boolean esAnho(){
+		if (anho<0)
+			return false;
+		else
+			return true;
+	}
+	
+	public boolean esBisiesto(){
+		if (anho%4==0 && anho%100 != 0)
+			return true;
+		else
+			return false;
+	}
+}
---
+++ src/test/java/AnhoTest.java
@@ -0,0 +1,55 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+
+public class AnhoTest {
+	Anho anho;
+	
+	@Before
+	public void iniciando(){
+		anho = new Anho();
+	}
+	
+	@Test
+	public void valorVacio() {
+		assertEquals(false, anho.esAnho());
+	}
+	
+	@Test
+	public void anhoNegativo() {
+		anho.setAnho(-165);
+		assertEquals(false, anho.esAnho());
+	}
+	
+	@Test
+	public void anhoPositivo() {
+		anho.setAnho(3);
+		assertEquals(true, anho.esAnho());
+	}
+	
+	@Test
+	public void anhoNoDivisbleEntre4() {
+		anho.setAnho(3);
+		assertEquals(false, anho.esBisiesto());
+	}
+	
+	@Test
+	public void anhoDivisbleEntre4() {
+		anho.setAnho(4);
+		assertEquals(true, anho.esBisiesto());
+	}
+	
+	@Test
+	public void anhoDivisbleEntre4yNo100() {
+		anho.setAnho(4);
+		assertEquals(true, anho.esBisiesto());
+	}
+	
+	@Test
+	public void anhoDivisbleEntre4y100() {
+		anho.setAnho(200);
+		assertEquals(false, anho.esBisiesto());
+	}
+}
