053035082111e8767bd4e0017eba0faeb75f867e
Paolo Adrian Fernandez Roman <piro_pafr@hotmail.com>
2013-12-03 08:38:51 -0400

    Respuesta 5

--- src/main/java/AnhoServlet.java
+++ src/main/java/AnhoServlet.java
@@ -8,12 +8,20 @@ import javax.servlet.http.HttpServletResponse;
 
 
 public class AnhoServlet extends HttpServlet {
-	
+	Anho anho = new Anho();
 	@Override
 	protected void service(HttpServletRequest request,
 			HttpServletResponse response) throws IOException, ServletException {
-		String anho = request.getParameter("anho");
+		String year = request.getParameter("anho");
 		response.getWriter().println("Este es el anho ingresado: " + '\n'
 				+ anho);
+		anho.setAnho(Integer.parseInt(year));
+		if(anho.esAnho())
+			if(anho.esBisiesto())
+				response.getWriter().println("El anho es bisiesto");
+			else
+				response.getWriter().println("El anho no es bisiesto");
+		else
+			response.getWriter().println("El anho no es valido");
 	}
 }
